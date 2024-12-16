196f0ca372b7a1b4ae9a8d246266ecd81554868e
KARTHIK NAIR <168218925+Karthik00769@users.noreply.github.com>
2024-12-16 17:01:13 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/file.jsx

--- src/file.jsx
+++ src/file.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
-import { faUser } from '@fontawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import MetaPassLogo from './MetaPassLogo.png'; 
 import SignatureIcon from './SignatureIcon.png'; 
 
