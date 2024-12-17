e115b567198b03ece3cad8274c6322cbeee80298
HARISH130505 <harish13.05.05@gmail.com>
2024-12-17 05:30:28 +0530

    initial commit from harish

--- src/User.jsx
+++ src/User.jsx
@@ -1,42 +1,63 @@
-import Userico from './assets/user.png'
 import { Link } from 'react-router-dom';
 import Nav from './Nav';
+import React,{useState} from 'react';
 function User(){
-    const Details=[
-        {
-            cn:'Republic of India',
-            passtype:'Ordinary',
-            passno:'H3547821',
-            name:'Harish J',
-            dob:'13-05-2005',
-            place:'Chennai',
-            country:'India',
-            doi:'XX-XX-XXXX',
-            doe:'XX-XX-XXXX',
-            auth:'Ministry of External Affairs',
-        },
-    ];
+    const [fn,setFn]=useState("");
+    const [ln,setLn]=useState("");
+    const [passno,setPassno]=useState("");
+    const [dob,setDob]=useState("");
+    const [pob,setPob]=useState("");
+
+    function handleFn(e){
+        setFn(e.target.value)
+    }
+    function handleLn(e){
+        setLn(e.target.value)
+    }
+    function handlePassno(e){
+        setPassno(e.target.value)
+    }
+    function handleDob(e){
+        setDob(e.target.value)
+    }
+    function handlePob(e){
+        setPob(e.target.value)
+    }
     return(
         <div>
             <Nav/>
-        <div className="text-white flex  justify-center items-center flex-col ">
+        <div className="text-white flex justify-center items-center flex-col my-10">
             <div className='bg-slate-600 rounded-xl bg-opacity-45 flex flex-col justify-center items-center py-4'>
             <h1 className='text-4xl font-semibold text-white'>User Details</h1>
             <div className="flex flex-col justify-center items-center lg:flex-row space-x-20">
-                <img src={Userico} className='w-32 lg:hidden pt-2'></img>
-                {Details.map((det,index)=>(
-                    <ul key={index} className="text-left font-robo py-4 md:text-xl mt-8">
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Country's Name: </span>{det.cn}</li>
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Passport Type:  </span>{det.passtype}</li>
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Passport Number: </span>{det.passno}</li>
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Holder's Name: </span>{det.name}</li>
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Date of Birth: </span> {det.dob}</li>
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Place of Birth: </span> {det.place}, {det.country}</li>
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Date of Issue: </span> {det.doi}</li>
-                        <li className='py-2'><span className='font-semibold text-gray-300'>Date of Expiry: </span> {det.doe}</li>
+                    <ul className="text-left font-robo p-4 md:text-xl mt-8 text-white">
+                        <li className='py-2 grid grid-cols-2 gap-0'>
+                            <span className='font-semibold text-gray-300'>First Name:</span>
+                            <input type="text" value={fn} placeholder="Enter First Name" onChange={handleFn} className='rounded-md p-2 bg-transparent border-solid border-2 border-slate-400'></input>
+                        </li>
+                        <li className='py-2 grid grid-cols-2 gap-0'>
+                            <span className='font-semibold text-gray-300'>Last Name:</span>
+                            <input type="text" value={ln} placeholder="Enter Last Name" onChange={handleLn}  className='rounded-md p-2 bg-transparent border-solid border-2 border-slate-400'></input>
+                        </li>
+                        <li className='py-2 grid grid-cols-2 gap-0'>
+                            <span className='font-semibold text-gray-300'>Passport Number:</span>
+                            <input type="text" value={passno} placeholder="Enter Passport number" onChange={handlePassno}  className='rounded-md p-2 bg-transparent border-solid border-2 border-slate-400'></input>
+                        </li>
+                        <li className='py-2 grid grid-cols-2 gap-0'>
+                            <span className='font-semibold text-gray-300'>Date of Birth:</span>
+                            <input type="date" value={dob} onChange={handleDob}  className='rounded-md p-2 bg-transparent border-solid border-2  border-slate-400'></input>
+                        </li>
+                        <li className='py-2 grid grid-cols-2 gap-0'>
+                            <span className='font-semibold text-gray-300'>Place Of Birth:</span>
+                            <select value={pob} onChange={handlePob}  className='rounded-md p-2 bg-transparent border-solid border-2 border-slate-400'>
+                                <option value={""} className='text-black'>Select Country</option>
+                                <option value={"India"}  className='text-black'>India</option>
+                                <option value={"USA"}  className='text-black'>USA</option>
+                                <option value={"UAE"}  className='text-black'>UAE</option>
+                                <option value={"Russia"}  className='text-black'>Russia</option>
+                            </select>
+                        </li>
                     </ul>
-                ))}
-                <img src={Userico} className='hidden lg:block w-80 object-cover'></img>
             </div>
             <Link to="/preview">
                 <button className='w-52 px-6 py-3 mt-5 text-white bg-slate-600 rounded-xl'>Save Details</button>
